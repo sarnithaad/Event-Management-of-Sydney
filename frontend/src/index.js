a5033ad080dc5ec0c2b27c45f51ec4c993529bbf
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-11 15:08:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+// Set the document title for accessibility & SEO
+document.title = "Louder World: Sydney Events";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
